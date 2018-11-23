91157899a33393d4d633e1c2913c4cd8366a7108
Cristiwan <crisgalliano1998@gmail.com>
2018-11-23 20:30:11 +0000

    began to implement basic hand functionality

---
+++ 1066 Tears To Many Mothers/Assets/Scripts/CardScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour
+{
+    public Vector3 cardPosition;
+    private int clickCount = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        cardPosition = gameObject.transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnMouseEnter()
+    {
+        if (clickCount == 0)
+        {
+            cardPosition.y += 200;
+            gameObject.transform.localPosition = cardPosition;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (clickCount == 0)
+        {
+            cardPosition.y -= 200;
+            gameObject.transform.localPosition = cardPosition;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        clickCount++;
+        if (clickCount == 1)
+        {
+            cardPosition.y = 500;
+            cardPosition.z = 100;
+            gameObject.transform.localPosition = cardPosition;
+        }
+        else
+        {
+            cardPosition.y = 200;
+            cardPosition.z = 0;
+            gameObject.transform.localPosition = cardPosition;
+            clickCount = 0;
+        }
+    }
+}
---
+++ 1066 Tears To Many Mothers/Assets/Scripts/HandScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandScript : MonoBehaviour
+{
+    public Vector3 handPosition;
+	// Use this for initialization
+	void Start ()
+    {
+        handPosition = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private void OnMouseEnter()
+    {
+        handPosition.z -= 200;
+        gameObject.transform.position = handPosition;
+    }
+
+    private void OnMouseExit()
+    {
+        handPosition.z += 200;
+        gameObject.transform.position = handPosition;
+    }
+}
