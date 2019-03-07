50e1168156b52134f46f84efe6d5f3721f2eb5a8
HINDL3Y <38388438+HINDL3Y@users.noreply.github.com>
2019-03-07 13:10:51 +0000

    Added placeholder images for main menu and added BGM to main menu

---
+++ 1066 Tears To Many Mothers/Assets/Scripts/FadeAlphaImagesMain.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeAlphaImagesMain : MonoBehaviour
+{
+    public Image imageOne;
+   //public Image imageTwo;
+    //public Image imageThree;
+    //public Image imageFour;
+
+
+    void Start()
+    {
+        imageOne = GetComponent<Image>();
+        Color a = imageOne.color;
+        a.a = 0;
+        imageOne.color = a;
+
+        /*imageTwo = GetComponent<Image>();
+        Color b = imageTwo.color;
+        b.a = 0;
+        imageTwo.color = b;
+
+        imageThree = GetComponent<Image>();
+        Color c = imageThree.color;
+        c.a = 0;
+        imageThree.color = c;
+
+        imageFour = GetComponent<Image>();
+        Color d = imageFour.color;
+        d.a = 0;
+        imageFour.color = d; */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
