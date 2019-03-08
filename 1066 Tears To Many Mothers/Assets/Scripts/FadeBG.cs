12e457cf2f0ed573faa7ad767153768c4ac54c5c
Cristiwan <crisgalliano1998@gmail.com>
2019-03-08 15:58:39 +0000

    Modified main menu UI
    centered all UI elements, mad sure images werent being stretched, replaced old fading system to script based one(now only need to add to arry of sprites to add new image), added greyscale shader.
    
    Co-Authored-By: hindl3y <hindl3y@users.noreply.github.com>

---
+++ 1066 Tears To Many Mothers/Assets/Scripts/FadeBG.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeBG : MonoBehaviour
+{
+    public Sprite[] backgroundImages;
+    public Image thisImage;
+    public Animator animator;
+    private int num = 0;
+    private int imageNum = 0;
+    public AnimationClip[] animations;
+    public float imageDisplayTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(fade());
+    }
+
+    private void animParameter()
+    {
+        num++;
+        if (num > 1)
+        {
+            num = 0;
+        }
+        animator.SetInteger("SwitchFade",num);
+    }
+
+    private void changeImage()
+    {
+        imageNum++;
+        if (imageNum > backgroundImages.Length - 1)
+        {
+            imageNum = 0;
+        }
+        thisImage.sprite = backgroundImages[imageNum];
+    }
+
+    private IEnumerator fade()
+    {
+        yield return new WaitForSeconds(animations[num].length);
+        if (num == 0)
+        {
+            yield return new WaitForSeconds(imageDisplayTime);
+        }
+        animParameter();
+        if (num == 0)
+        {
+            changeImage();
+        }
+        StartCoroutine(fade());
+    }
+}
